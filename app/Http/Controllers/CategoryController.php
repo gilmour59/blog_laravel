08c22a84bff1af347c1e8876b66aa1bd5b40ad97
gilmour59 <gilmouralmalbisdev@gmail.com>
2019-04-21 00:26:44 +0800

    added requests

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Category;
+use App\Http\Requests\Categories\CreateCategoryRequest;
+use App\Http\Requests\Categories\UpdateCategoryRequest;
 
 class CategoryController extends Controller
 {
@@ -33,12 +35,8 @@ class CategoryController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreateCategoryRequest $request)
     {
-        $this->validate($request, [
-            'name' => 'required|unique:categories'
-        ]);
-
         Category::create([
             'name' => $request->name
         ]);
@@ -65,9 +63,9 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Category $category)
     {
-        //
+        return view('categories.create')->with('category', $category);
     }
 
     /**
@@ -77,9 +75,15 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UpdateCategoryRequest $request, Category $category)
     {
-        //
+        $category->update([
+            'name' => $request->name
+        ]);
+
+        session()->flash('success', 'Category Updated');
+
+        return redirect(route('categories.index'));
     }
 
     /**
--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -3,7 +3,11 @@
 @section('content')
     <div class="card card-default">
         <div class="card-header">
-            Create Category
+            @isset($category)
+                Edit Category
+            @else
+                Create Category
+            @endisset
         </div>
         <div class="card-body">
             @if ($errors->any())
@@ -15,15 +19,18 @@
                     @endforeach
                 </div>
             @endif
-            <form action="{{ route('categories.store') }}" method="POST">
+            <form action="{{ isset($category) ? route('categories.update', $category->id) : route('categories.store') }}" method="POST">
                 @csrf
                 <div class="form-group">
                     <label for="name">Name:</label>
-                    <input type="text" name="name" id="name" class="form-control">
+                    <input type="text" name="name" id="name" class="form-control" value="{{ isset($category) ? $category->name : ""}}">
                 </div>
                 <div class="form-group">
                     <input type="submit" value="Enter" class="btn btn-primary float-right">
                 </div>
+                    @isset($category)
+                        @method('PUT')
+                    @endisset
             </form>
         </div>
     </div>
