51812e8b331244b52801edc6e0ec8be19b3ef38a
Gilmour Almalbis <gilmouralmalbisdev@gmail.com>
2019-04-29 13:18:15 +0800

    done with old input (categories)

--- resources/views/categories/create_edit.blade.php
+++ resources/views/categories/create_edit.blade.php
@@ -23,7 +23,7 @@
                 @csrf
                 <div class="form-group">
                     <label for="name">Name:</label>
-                    <input type="text" name="name" id="name" class="form-control" value="{{ isset($category) ? $category->name : ""}}">
+                    <input type="text" name="name" id="name" class="form-control" value="{{ old('name') ? old('name') : (isset($category) ? $category->name : "") }}">
                 </div>
                 <div class="form-group">
                     <input type="submit" value="Enter" class="btn btn-primary float-right">
